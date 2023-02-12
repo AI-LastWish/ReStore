d5851d3c5d427d12bba9e293d807322fb954a57a
Nguyen Quang Tri <nguyenquangtri@Nguyens-Mac-mini.local>
2023-02-12 17:17:25 +0700

    Section 3: React Basic

---
+++ client/src/features/catalog/Catalog.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import { Product } from "../../app/models/Product";
+import ProductList from "./ProductList";
+
+const Catalog = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  );
+};
+
+export default Catalog;
---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,53 @@
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Product } from "../../app/models/Product";
+import { Avatar, CardHeader } from "@mui/material";
+
+interface Props {
+  product: Product;
+}
+
+const ProductCard = ({ product }: Props) => {
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: "bold", color: "primary.main" },
+        }}
+      />
+      <CardMedia
+        sx={{
+          height: 140,
+          backgroundSize: "contain",
+          bgcolor: "primary.light",
+        }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom color="secondary" variant="h5">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add To Cart</Button>
+        <Button size="small">View</Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default ProductCard;
