974910ffe73179ceb6177c5489f77355c1f6eaa6
Nguyen Quang Tri <nguyenquangtri@Nguyens-Mac-mini.local>
2023-02-18 20:21:19 +0700

    Section 8: Paging, Sorting, Filtering

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,20 +1,29 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { toast } from "react-toastify";
+import { PaginatedResponse } from "../models/pagination";
+
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 500));
 
 axios.defaults.baseURL = "http://localhost:5000/api/";
 axios.defaults.withCredentials = true;
 
-const sleep = () => new Promise((resolve) => setTimeout(resolve, 1000));
-
 const responseBody = (response: AxiosResponse) => response.data;
 
 axios.interceptors.response.use(
-  async (res) => {
+  async (response) => {
     await sleep();
-    return res;
+    const pagination = response.headers["pagination"];
+    if (pagination) {
+      response.data = new PaginatedResponse(
+        response.data,
+        JSON.parse(pagination)
+      );
+      return response;
+    }
+    return response;
   },
   (error: AxiosError) => {
-    const { data, status } = error.response! as AxiosResponse;
+    const { data, status } = error.response as AxiosResponse;
     switch (status) {
       case 400:
         if (data.errors) {
@@ -31,19 +40,13 @@ axios.interceptors.response.use(
       case 401:
         toast.error(data.title);
         break;
-      case 403:
-        toast.error("You are not allowed to do that!");
-        break;
       case 500:
         window.location.replace("/server-error");
-        // history.push({
-        //   pathname: "/server-error",
-        //   state: { error: data },
-        // });
         break;
       default:
         break;
     }
+
     return Promise.reject(error.response);
   }
 );
@@ -53,23 +56,11 @@ const requests = {
     axios.get(url, { params }).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
   put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-  delete: (url: string) => axios.delete(url).then(responseBody),
-  postForm: (url: string, data: FormData) =>
-    axios
-      .post(url, data, {
-        headers: { "Content-type": "multipart/form-data" },
-      })
-      .then(responseBody),
-  putForm: (url: string, data: FormData) =>
-    axios
-      .put(url, data, {
-        headers: { "Content-type": "multipart/form-data" },
-      })
-      .then(responseBody),
+  del: (url: string) => axios.delete(url).then(responseBody),
 };
 
 const Catalog = {
-  list: () => requests.get("products"),
+  list: (params: URLSearchParams) => requests.get("products", params),
   details: (id: number) => requests.get(`products/${id}`),
   fetchFilters: () => requests.get("products/filters"),
 };
@@ -87,7 +78,7 @@ const Basket = {
   addItem: (productId: number, quantity = 1) =>
     requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
   removeItem: (productId: number, quantity = 1) =>
-    requests.delete(`basket?productId=${productId}&quantity=${quantity}`),
+    requests.del(`basket?productId=${productId}&quantity=${quantity}`),
 };
 
 const agent = {
